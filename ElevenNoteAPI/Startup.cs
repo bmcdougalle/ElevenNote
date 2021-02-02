32499c36964eaaad7c18ffc818be72862299596c
bmcdougalle <bmcdougalle@gmail.com>
2021-02-02 00:46:46 -0500

    Added Cors

--- ElevenNoteAPI/Startup.cs
+++ ElevenNoteAPI/Startup.cs
@@ -12,6 +12,7 @@ namespace ElevenNoteAPI
     {
         public void Configuration(IAppBuilder app)
         {
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
             ConfigureAuth(app);
         }
     }
